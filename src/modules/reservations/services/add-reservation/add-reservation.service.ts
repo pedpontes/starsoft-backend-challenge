3b0cf61f0ad8a9ca3e3b4c5491c93f4ab3fccc07
Pedro Pontes <pontespeter@gmail.com>
2026-02-16 11:57:33 -0300

    refactor: improve logic add reservation service

--- src/modules/reservations/services/add-reservation/add-reservation.service.ts
+++ src/modules/reservations/services/add-reservation/add-reservation.service.ts
@@ -4,53 +4,32 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { CreateReservationDto } from '../../dtos/create-reservation.dto';
-import { EventsService } from '../../../../shared/events/usecases/events.service';
 import { ReservationRepository } from '../../repositories/contracts/reservation.repository';
 import { SessionRepository } from '../../../sessions/repositories/contracts/session.repository';
+import { Seat } from 'src/modules/seats/entities/seat.entity';
 
 @Injectable()
 export class AddReservationService {
+  #EXPIRE_SEC = 30_000;
+
   constructor(
     private readonly reservationRepository: ReservationRepository,
     private readonly sessionRepository: SessionRepository,
-    private readonly eventsService: EventsService,
   ) {}
 
   async addReservation(dto: CreateReservationDto) {
     const seatIds = [...new Set(dto.seatIds)].sort();
-    if (seatIds.length !== dto.seatIds.length) {
+    if (seatIds.length !== dto.seatIds.length)
       throw new BadRequestException('Duplicated seats in request.');
-    }
-
-    const expiresAt = new Date(Date.now() + 30_000);
 
     const seats = await this.sessionRepository.loadSeatsBySessionId(
       dto.sessionId,
     );
-    const seatIdSet = new Set(seats.map((seat) => seat.id));
-    const missingSeats = seatIds.filter((seatId) => !seatIdSet.has(seatId));
-    if (missingSeats.length > 0) {
-      throw new BadRequestException(
-        'Some seats were not found for this session.',
-      );
-    }
 
-    const soldSeatIds = await this.sessionRepository.loadSoldSeatIds(
-      dto.sessionId,
-      seatIds,
-    );
-    if (soldSeatIds.length > 0) {
-      throw new ConflictException('Some seats are already sold.');
-    }
+    this.ensureExistsSeats(seats, seatIds);
+    await this.ensureSeatsAvaibility(dto.sessionId, seatIds);
 
-    const reservedSeatIds = await this.sessionRepository.loadReservedSeatIds(
-      dto.sessionId,
-      seatIds,
-    );
-    if (reservedSeatIds.length > 0) {
-      throw new ConflictException('Some seats are already reserved.');
-    }
-
+    const expiresAt = new Date(Date.now() + this.#EXPIRE_SEC);
     const reservation = await this.reservationRepository.add({
       sessionId: dto.sessionId,
       userId: dto.userId,
@@ -58,14 +37,28 @@ export class AddReservationService {
       expiresAt,
     });
 
-    await this.eventsService.publish('reservation.created', {
-      reservationId: reservation.id,
-      sessionId: dto.sessionId,
-      userId: dto.userId,
-      seatIds,
-      expiresAt: expiresAt.toISOString(),
-    });
-
     return reservation;
+  }
+
+  private ensureExistsSeats(seats: Seat[], seatIds: string[]) {
+    const seatIdSet = new Set(seats.map((seat) => seat.id));
+    const missingSeats = seatIds.filter((seatId) => !seatIdSet.has(seatId));
+
+    if (missingSeats.length > 0)
+      throw new BadRequestException(
+        'Some seats were not found for this session.',
+      );
+  }
+
+  private async ensureSeatsAvaibility(sessionId: string, seatIds: string[]) {
+    const [soldSeatIds, reservedSeatIds] = await Promise.all([
+      this.sessionRepository.loadSoldSeatIds(sessionId, seatIds),
+      this.sessionRepository.loadReservedSeatIds(sessionId, seatIds),
+    ]);
+
+    if (soldSeatIds.length > 0)
+      throw new ConflictException('Some seats are already sold.');
+    if (reservedSeatIds.length > 0)
+      throw new ConflictException('Some seats are already reserved.');
   }
 }
